61ec706552f814012bb1bb2e0cb62d9f85381899
EsneiderLoaiza <esneiderlvs@gmail.com>
2022-10-30 13:46:59 -0500

    feat: Se definen entidades de moduleeventmanager

--- src/main/java/co/com/ies/smolplus/context/moduleeventmanager/insfrastructure/secundary/counterevent/CounterEventEntity.java
+++ src/main/java/co/com/ies/smolplus/context/moduleeventmanager/insfrastructure/secundary/counterevent/CounterEventEntity.java
@@ -1,3 +1,127 @@
 package co.com.ies.smolplus.context.moduleeventmanager.insfrastructure.secundary.counterevent;
 
-public class CounterEventEntity {}
+import co.com.ies.smolplus.context.moduleeventmanager.insfrastructure.secundary.eventdevice.EventDeviceEntity;
+import java.io.Serializable;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity
+@Table(name = "counter_event")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+@SuppressWarnings("common-java:DuplicatedBlocks")
+public class CounterEventEntity implements Serializable {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "valueCounter")
+  private Long valueCounter;
+
+  @Column(name = "denominationSale")
+  private Float denominationSale;
+
+  @ManyToOne(optional = false)
+  @NotNull
+  private CounterTypeEntity counterType; //Definir entidad en su respectivo modulo
+
+  @ManyToOne(optional = false)
+  @NotNull
+  private EventDeviceEntity eventDevice;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getValueCounter() {
+    return valueCounter;
+  }
+
+  public void setValueCounter(Long valueCounter) {
+    this.valueCounter = valueCounter;
+  }
+
+  public Float getDenominationSale() {
+    return denominationSale;
+  }
+
+  public void setDenominationSale(Float denominationSale) {
+    this.denominationSale = denominationSale;
+  }
+
+  public CounterTypeEntity getCounterType() {
+    return counterType;
+  }
+
+  public void setCounterType(CounterTypeEntity counterType) {
+    this.counterType = counterType;
+  }
+
+  public EventDeviceEntity getEventDevice() {
+    return eventDevice;
+  }
+
+  public void setEventDevice(EventDeviceEntity eventDevice) {
+    this.eventDevice = eventDevice;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + ((valueCounter == null) ? 0 : valueCounter.hashCode());
+    result = prime * result + ((denominationSale == null) ? 0 : denominationSale.hashCode());
+    result = prime * result + ((counterType == null) ? 0 : counterType.hashCode());
+    result = prime * result + ((eventDevice == null) ? 0 : eventDevice.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    CounterEventEntity other = (CounterEventEntity) obj;
+    if (id == null) {
+      if (other.id != null) return false;
+    } else if (!id.equals(other.id)) return false;
+    if (valueCounter == null) {
+      if (other.valueCounter != null) return false;
+    } else if (!valueCounter.equals(other.valueCounter)) return false;
+    if (denominationSale == null) {
+      if (other.denominationSale != null) return false;
+    } else if (!denominationSale.equals(other.denominationSale)) return false;
+    if (counterType == null) {
+      if (other.counterType != null) return false;
+    } else if (!counterType.equals(other.counterType)) return false;
+    if (eventDevice == null) {
+      if (other.eventDevice != null) return false;
+    } else if (!eventDevice.equals(other.eventDevice)) return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return (
+      "CounterEventEntity [id=" +
+      id +
+      ", valueCounter=" +
+      valueCounter +
+      ", denominationSale=" +
+      denominationSale +
+      ", counterType=" +
+      counterType +
+      ", eventDevice=" +
+      eventDevice +
+      "]"
+    );
+  }
+}
--- src/main/java/co/com/ies/smolplus/context/moduleeventmanager/insfrastructure/secundary/eventdevice/EventDeviceEntity.java
+++ src/main/java/co/com/ies/smolplus/context/moduleeventmanager/insfrastructure/secundary/eventdevice/EventDeviceEntity.java
@@ -1,3 +1,144 @@
 package co.com.ies.smolplus.context.moduleeventmanager.insfrastructure.secundary.eventdevice;
 
-public class EventDeviceEntity {}
+import co.com.ies.smolplus.context.moduleeventmanager.insfrastructure.secundary.eventtype.EventTypeEntity;
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+@Entity
+@Table(name = "event_device")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class EventDeviceEntity implements Serializable {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "id")
+  private Long id;
+
+  @Column(name = "createdAt")
+  private ZonedDateTime createdAt;
+
+  @Column(name = "theoreticalPercentage")
+  private Boolean theoreticalPercentage;
+
+  @Column(name = "moneyDenomination")
+  private Double moneyDenomination;
+
+  @ManyToOne(optional = false)
+  @NotNull
+  private EstablishmentEntity establishment; //Definir entidad en su respectivo modulo
+
+  @ManyToOne(optional = false)
+  @NotNull
+  private EventTypeEntity deviceCategory;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public ZonedDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(ZonedDateTime createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Boolean getTheoreticalPercentage() {
+    return theoreticalPercentage;
+  }
+
+  public void setTheoreticalPercentage(Boolean theoreticalPercentage) {
+    this.theoreticalPercentage = theoreticalPercentage;
+  }
+
+  public Double getMoneyDenomination() {
+    return moneyDenomination;
+  }
+
+  public void setMoneyDenomination(Double moneyDenomination) {
+    this.moneyDenomination = moneyDenomination;
+  }
+
+  public EstablishmentEntity getEstablishment() {
+    return establishment;
+  }
+
+  public void setEstablishment(EstablishmentEntity establishment) {
+    this.establishment = establishment;
+  }
+
+  public EventTypeEntity getDeviceCategory() {
+    return deviceCategory;
+  }
+
+  public void setDeviceCategory(EventTypeEntity deviceCategory) {
+    this.deviceCategory = deviceCategory;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + ((createdAt == null) ? 0 : createdAt.hashCode());
+    result = prime * result + ((theoreticalPercentage == null) ? 0 : theoreticalPercentage.hashCode());
+    result = prime * result + ((moneyDenomination == null) ? 0 : moneyDenomination.hashCode());
+    result = prime * result + ((establishment == null) ? 0 : establishment.hashCode());
+    result = prime * result + ((deviceCategory == null) ? 0 : deviceCategory.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) return true;
+    if (obj == null) return false;
+    if (getClass() != obj.getClass()) return false;
+    EventDeviceEntity other = (EventDeviceEntity) obj;
+    if (id == null) {
+      if (other.id != null) return false;
+    } else if (!id.equals(other.id)) return false;
+    if (createdAt == null) {
+      if (other.createdAt != null) return false;
+    } else if (!createdAt.equals(other.createdAt)) return false;
+    if (theoreticalPercentage == null) {
+      if (other.theoreticalPercentage != null) return false;
+    } else if (!theoreticalPercentage.equals(other.theoreticalPercentage)) return false;
+    if (moneyDenomination == null) {
+      if (other.moneyDenomination != null) return false;
+    } else if (!moneyDenomination.equals(other.moneyDenomination)) return false;
+    if (establishment == null) {
+      if (other.establishment != null) return false;
+    } else if (!establishment.equals(other.establishment)) return false;
+    if (deviceCategory == null) {
+      if (other.deviceCategory != null) return false;
+    } else if (!deviceCategory.equals(other.deviceCategory)) return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return (
+      "EventDeviceEntity [id=" +
+      id +
+      ", createdAt=" +
+      createdAt +
+      ", theoreticalPercentage=" +
+      theoreticalPercentage +
+      ", moneyDenomination=" +
+      moneyDenomination +
+      ", establishment=" +
+      establishment +
+      ", deviceCategory=" +
+      deviceCategory +
+      "]"
+    );
+  }
+}
