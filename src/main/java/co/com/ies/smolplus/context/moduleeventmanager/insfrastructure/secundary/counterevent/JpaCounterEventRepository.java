8c21e19bca4fd222d7b9d6b3e829583148f5e823
esneider-ies- <david.loaiza@iesonline.com.co>
2022-10-31 09:34:38 -0500

    refactor: Se cambia tipo de dato para Jpa de Counterevent y EventDevice

--- src/main/java/co/com/ies/smolplus/context/moduleeventmanager/insfrastructure/secundary/counterevent/JpaCounterEventRepository.java
+++ src/main/java/co/com/ies/smolplus/context/moduleeventmanager/insfrastructure/secundary/counterevent/JpaCounterEventRepository.java
@@ -1,7 +1,9 @@
 package co.com.ies.smolplus.context.moduleeventmanager.insfrastructure.secundary.counterevent;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JpaCounterEventRepository extends JpaRepository<CounterEventEntity, Long>, JpaSpecificationExecutor<CounterEventEntity> {}
+public interface JpaCounterEventRepository extends JpaRepository<CounterEventEntity, UUID>, JpaSpecificationExecutor<CounterEventEntity> {}
--- src/main/java/co/com/ies/smolplus/context/moduleeventmanager/insfrastructure/secundary/eventdevice/JpaEventDeviceRepository.java
+++ src/main/java/co/com/ies/smolplus/context/moduleeventmanager/insfrastructure/secundary/eventdevice/JpaEventDeviceRepository.java
@@ -1,7 +1,9 @@
 package co.com.ies.smolplus.context.moduleeventmanager.insfrastructure.secundary.eventdevice;
 
+import java.util.UUID;
+
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface JpaEventDeviceRepository extends JpaRepository<EventDeviceEntity, Long>, JpaSpecificationExecutor<EventDeviceEntity> {}
+public interface JpaEventDeviceRepository extends JpaRepository<EventDeviceEntity, UUID>, JpaSpecificationExecutor<EventDeviceEntity> {}
